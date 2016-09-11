81a12bf0b02068701b95eeb2c34c78e083838e33
Marcode777 <marcosebello7@gmail.com>
2016-09-11 11:35:29 -0400

    deleted animation.css, replaced with styles.css, modified src/index.html, src/js/animation.js, src/js/client.js and made background image full-screen

--- src/js/animation.js
+++ src/js/animation.js
@@ -1,1 +1,5 @@
 TweenMax.to(".first",3.5, {x:500, ease:Back.easeOut});
+TweenMax.from(".grid",10, {x:1500, delay:2, ease:Back.easeOut});
+// TweenMax.to(".why",4, {x:500, ease:Back.easeOut});
+// TweenMax.to(".first",4.5, {x:500, ease:Back.easeOut});
+// TweenMax.to(".first",5, {x:500, ease:Back.easeOut});
--- src/js/client.js
+++ src/js/client.js
@@ -10,9 +10,7 @@ class Layout extends React.Component{
     return(
       <div>
         <Top/>
-        <Middle/>
         <Base/>
-
       </div> 
       );
   }
@@ -23,36 +21,26 @@ var Top = React.createClass({
     return(
       <div>
         <h1 className="first" >JAVASCRIPTNYC</h1>
-        <img src={"https://s3.amazonaws.com/s3.imagefinder.co/uploads/2016/01/20143942/boss-fight-free-high-quality-stock-images-photos-photography-new-york-city-skyline-960x640.jpg"} />
-        <form action="process.php" method="POST"> 
-          Name<input type="text" name="your_name" size="20" value="" required/>
-          Email<input type="email" name="email" size="20" value="" required/>
-          <input type="submit" value="submit" name="submit"/>
-        </form>
-        <div>
-        <p>text</p>
-        </div>
+        <div className="grid">
+          <div  class="col-md-4"><h2 className="why">Why JAVASCRIPTNYC?</h2><h3>Because javascript and its omnipotence has finally been realized and we are the ONLY javascript-focused conference in all of New York City</h3></div>
+          <div class="col-md-4"><h2 className="what">What is in it?</h2><h3>Each year JAVASCRIPTNYC has a theme focused around javascript, ranging from the most common and helpful uses of javascript to the most cutting edge of what is coming next, with top guest speakers ready to share their thoughts and answer your questions.</h3></div>
+          <div class="col-md-4"><h2 className="when">When will it be held?</h2><h3>Stay tuned, or sign up for more information</h3></div>
+        </div> 
       </div> 
       )
   }
 })
 
-var Middle = React.createClass({
-  render:function(){
-    return(
-      <div className="grid">
-      <div className="why" class="col-md-4"><h2>WHY JAVASCRIPTNYC?</h2><h3>Because javascript and its omnipotence has finally been realized and we are the ONLY javascript-focused conference in all of New York City</h3></div>
-      <div className="what"class="col-md-4"><h2>What is in it?</h2><h3>Each year JAVASCRIPTNYC has a theme focused around javascript, ranging from the most common and helpful uses of javascript to the most cutting edge of what is coming next, with top guest speakers ready to share their thoughts and answer your questions.</h3></div>
-      <div className="when"class="col-md-4"><h2>When will it be held?</h2><h3>Stay tuned, or sign up for more information</h3></div>
-      </div>
-      )
-  }
-})
+
 
 var Base = React.createClass({
   render: function() {
     return (
-      <p>some more text</p>
+        <form action="process.php" method="POST"> 
+          Name<input type="text" name="your_name" size="20" value="" required/>
+          Email<input type="email" name="email" size="20" value="" required/>
+          <input type="submit" value="submit" name="submit"/>
+        </form>
     );
   }
 });
@@ -61,8 +49,12 @@ var Base = React.createClass({
 var jsStyle = {
   color: 'black',
   backgroundImage: 'url("https://s3.amazonaws.com/s3.imagefinder.co/uploads/2016/01/20143942/boss-fight-free-high-quality-stock-images-photos-photography-new-york-city-skyline-960x640.jpg")',
-  WebkitTransition: 'all', // note the capital 'W' here
-  msTransition: 'all' // 'ms' is the only lowercase vendor prefix
+  position:"absolute",
+  WebkitBackgroundSize: 'cover', // note the capital 'W' here
+  height:"100%",
+  width:"100%",
+  // WebkitTransition: 'all', // note the capital 'W' here
+  // msTransition: 'all' // 'ms' is the only lowercase vendor prefix
 };
 
 
